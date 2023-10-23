b231ec0911a6aea8dbfa380dd986c313901bc729
shubham <shubhamkashyap1919@gmail.com>
2023-10-23 17:11:22 +0530

    scrpit added

---
+++ script.js
@@ -0,0 +1,7 @@
+const beginner = () => {
+    window.location.href = "https://www.youtube.com/playlist?list=PLJB-E05ZNmO1aCXD4jC_xQ4RirpEcJnf3";
+}
+
+const intermediate = () => {
+    window.location.href = "https://www.youtube.com/playlist?list=PLJB-E05ZNmO1aCXD4jC_xQ4RirpEcJnf3";
+}
