54bd9a0f7dfa5619fc7bb5914c746e0a8ef4e791
shubham <shubhamkashyap1919@gmail.com>
2023-10-24 13:16:33 +0530

    funcs

--- script.js
+++ script.js
@@ -3,5 +3,12 @@ const beginner = () => {
 }
 
 const intermediate = () => {
-    window.location.href = "https://www.youtube.com/playlist?list=PLJB-E05ZNmO1aCXD4jC_xQ4RirpEcJnf3";
+    window.location.href = "https://www.youtube.com/playlist?list=PLp8Cocyu4SVgpL33B1wYtinHHtGfH4LTR";
+}
+
+const riskmanagement = () => {
+    window.location.href = "https://www.youtube.com/playlist?list=PLp8Cocyu4SVgYiyDh4OHsrj_B8MVAPcOQ";
+}
+const trading = () => {
+    window.location.href = "https://www.youtube.com/playlist?list=PLp8Cocyu4SVjhfSwjXn8ypvkpNDuDbcCs";
 }
